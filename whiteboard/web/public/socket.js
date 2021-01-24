915620106764cdf4a8ec7956f143ffaf3885eefc
krits2503 <khandelwalkriti9925@gmail.com>
2021-01-24 18:45:07 +0530

    update

---
+++ whiteboard/web/public/socket.js
@@ -0,0 +1,23 @@
+Socket.on("md",function(lineObject){
+    let currentStrokeStyle=ctx.strokeStyle;
+    let currentPenWidth=ctx.lineWidth;
+
+    ctx.strokeStyle=lineObject.color;
+    ctx.lineWidth=lineObject.width;
+
+    ctx.beginPath();
+    ctx.moveTo(lineObject.x,lineObject.y);
+});
+
+Socket.on("mm",function(lineObject){
+    let currentStrokeStyle=ctx.strokeStyle;
+    let currentPenWidth=ctx.lineWidth;
+    ctx.strokeStyle=lineObject.color;
+    ctx.lineWidth=lineObject.width;
+
+    ctx.lineTo(lineObject.x,lineObject.y);
+    ctx.stroke();
+
+    ctx.strokeStyle=currentStrokeStyle;
+    ctx.lineWidth=currentPenWidth;
+})
